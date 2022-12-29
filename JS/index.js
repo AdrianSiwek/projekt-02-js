52b93c6dd1875f346639185f4f746f263a51c884
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-12-29 18:16:10 +0100

    Update index.js

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+const upBtn = document.querySelector('.sizeUp');
+const downBtn = document.querySelector('.sizeDown');
+const colorBtn = document.querySelector('.color');
+const p = document.querySelector('p');
+
